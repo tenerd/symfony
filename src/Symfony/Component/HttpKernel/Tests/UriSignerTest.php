30692e34c193564d49a99ebe52456257afa986d3
Jakub Simon <Jakub.Simon@lmc.eu>
2015-03-16 21:39:42 +0100

    Fix UriSigner::buildUrl fixed correct http_build_query building with different arg_separator.output setting

--- src/Symfony/Component/HttpKernel/Tests/UriSignerTest.php
+++ src/Symfony/Component/HttpKernel/Tests/UriSignerTest.php
@@ -15,6 +15,21 @@ use Symfony\Component\HttpKernel\UriSigner;
 
 class UriSignerTest extends \PHPUnit_Framework_TestCase
 {
+    /** @var string backup original value for ini setting */
+    protected $argSeparatorOutputBackup;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->argSeparatorOutputBackup = ini_get('arg_separator.output');
+    }
+
+    protected function tearDown()
+    {
+        ini_set('arg_separator.output', $this->argSeparatorOutputBackup);
+        parent::tearDown();
+    }
+
     public function testSign()
     {
         $signer = new UriSigner('foobar');
@@ -33,6 +48,17 @@ class UriSignerTest extends \PHPUnit_Framework_TestCase
 
         $this->assertTrue($signer->check($signer->sign('http://example.com/foo')));
         $this->assertTrue($signer->check($signer->sign('http://example.com/foo?foo=bar')));
+
+        $this->assertTrue($signer->sign('http://example.com/foo?foo=bar&bar=foo') === $signer->sign('http://example.com/foo?bar=foo&foo=bar'));
+    }
+
+    public function testCheckWithDifferentArgSeparator()
+    {
+        $signer = new UriSigner('foobar');
+
+        ini_set('arg_separator.output', '&amp;');
+        $this->assertTrue($signer->check($signer->sign('http://example.com/foo')));
+        $this->assertTrue($signer->check($signer->sign('http://example.com/foo?foo=bar&baz=bay')));
 
         $this->assertTrue($signer->sign('http://example.com/foo?foo=bar&bar=foo') === $signer->sign('http://example.com/foo?bar=foo&foo=bar'));
     }
